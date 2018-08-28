b47272e9815d52ed04039c95b757ffaaf45a7ece
k3cman <kecman.design@gmail.com>
2018-08-28 20:51:19 +0200

    mern shopping list

---
+++ mern-shopping-list/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const items = require('./routes/api/items');
+const path = require('path');
+
+const app = express();
+
+//Bodyparser middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+app.use(bodyParser.json());
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+//Use Routes
+
+app.use('/api/items', items);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
